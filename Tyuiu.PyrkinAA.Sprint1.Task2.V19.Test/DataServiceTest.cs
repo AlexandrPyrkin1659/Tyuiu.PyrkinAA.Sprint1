cd1b9603f7344b7366292d89539ac0ad05a4db67
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-12-30 20:05:28 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint1.Task2.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task2.V19.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task2.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpresion()
+        {
+            DataService ds = new DataService();
+            int x = 3937;
+            var res = ds.ConvertInchToKm(x);
+            Assert.AreEqual(100, res);
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint1.Task2.V19/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task2.V19.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task2.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите растояние в дюймах X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Растояние в метрах X = " + ds.ConvertInchToKm(x));
+            Console.ReadLine();
+        }
+    }
+}
