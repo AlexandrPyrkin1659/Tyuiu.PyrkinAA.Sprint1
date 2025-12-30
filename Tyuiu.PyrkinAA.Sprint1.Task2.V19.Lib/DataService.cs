a47ab402c45f3853d6a5f25c47c557388deca2b0
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-12-30 20:29:09 +0500

    3

--- Tyuiu.PyrkinAA.Sprint1.Task2.V19.Lib/DataService.cs
+++ Tyuiu.PyrkinAA.Sprint1.Task2.V19.Lib/DataService.cs
@@ -5,7 +5,7 @@ namespace Tyuiu.PyrkinAA.Sprint1.Task2.V19.Lib
     {
         public double ConvertInchToKm(int value)
         {
-            return value / 39.37;
+            return Math.Round(value / 39.37, 3);
 
         }
     }
