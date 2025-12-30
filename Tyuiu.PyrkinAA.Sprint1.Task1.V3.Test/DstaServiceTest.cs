769c0dfd58ddb525987505e5ed5e1c22db671c2b
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-12-30 17:42:02 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint1.Task1.V3.Test/DstaServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task1.V3.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task1.V3.Test
+{
+    [TestClass]
+    public sealed class DstaServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res =ds.Calculate(x, y);
+            Assert.AreEqual(2.75, res);
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint1.Task1.V3/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task1.V3.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task1.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Пыркин А. А. | АСОиУБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                              *");
+            Console.WriteLine("* Выполнил: Пыркин Александр Артемьевич | АСОиУБ-25-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (x - y) / (x + 3) + 3 и печатает его     *");
+            Console.WriteLine("* на экране.                                                              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
