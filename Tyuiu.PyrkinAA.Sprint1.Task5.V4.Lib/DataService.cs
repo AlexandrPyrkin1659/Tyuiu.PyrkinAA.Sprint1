407ddf2e7581bd4e5019518776a2b7bc212142b5
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-12-30 23:21:30 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint1.Task5.V4.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.PyrkinAA.Sprint1.Task5.V4.Lib
+{
+    public class DataService : ISprint1Task5V4
+    {
+        public int SecondsToHours(int time)
+        {
+            return (time / 60 / 60);
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint1.Task5.V4/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task5.V4.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task5.V4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService dataService = new DataService();
+            int x;
+
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(dataService.SecondsToHours(x));
+        }
+    }
+}
