80e7b3d4ad33f24cb58c24e2bac25b969c44202d
Александр Пыркин <Tyuiu.PyrkinAA@outlook.com>
2025-12-30 23:56:13 +0500

    1

---
+++ Tyuiu.PyrkinAA.Sprint1.Task6.V2.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+using Tyuiu.PyrkinAA.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task6.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            string value = "Hello world";
+            bool result = ds.CheckHello(value);
+            Assert.IsTrue(result);
+        }
+    }
+}
---
+++ Tyuiu.PyrkinAA.Sprint1.Task6.V2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.PyrkinAA.Sprint1.Task6.V2.Lib;
+namespace Tyuiu.PyrkinAA.Sprint1.Task6.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("* Введите строку:                                                         *");
+            string value = Console.ReadLine();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CheckHello(value));
+            Console.ReadKey();
+        }
+    }
+}
